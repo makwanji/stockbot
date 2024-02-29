18aa95efa08169e0d1adfc3ec17de79fef22d112
Divyarajsinh01 <divyarajsinhrayjada3@gmail.com>
2024-02-29 17:05:19 +0530

    first commit

--- frontend/src/app/components/sample-component/sample-component.jsx
+++ frontend/src/app/components/sample-component/sample-component.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function callServer() {
-  axios.get(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/test`, {
-    params: {
-      table: 'sample',
-    },
-  }).then((response) => {
-    console.log(response.data);
-  });
-}
-
 export function SampleComponent() {
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    callServer()
+  }, [])
+
+  function callServer() {
+    axios.get(`http://localhost:${process.env.REACT_APP_SERVER_PORT}/test`, {
+      params: {
+        table: 'sample',
+      },
+    }).then((response) => {
+      console.log(response.data);
+      setData(response.data)
+      // return response.data
+    });
+  }
+
+  // callServer()
+
   return (
     <div>
       This is a sample component
-      {callServer()}
+      {/* {callServer()} */}
+      <table>
+        <thead>
+          <tr>
+            <td>No.</td>
+            <td>Name</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map((item, index) => (
+            <tr key={index}>
+              <td >
+                {item.id}{/* Assuming your data object has 'id' and 'name' properties */}
+              </td>
+              <td>
+                {item.name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
